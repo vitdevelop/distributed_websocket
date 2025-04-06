518a91c13aedc882ff393daa0be76648e56267f3
vitdevelop <balanici_vitalik@mail.ru>
2025-04-06 19:02:22 +0300

    Add reuse port config

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module distributed_websocket
 go 1.24
 
 require (
+	github.com/gobwas/ws v1.4.0
+	golang.org/x/sys v0.6.0
+)
+
+require (
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
-	github.com/gobwas/ws v1.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"context"
 	"distributed_websocket/handler"
+	"golang.org/x/sys/unix"
 	"log"
+	"net"
 	"net/http"
+	"syscall"
 )
 
 func main() {
+	ln := listenerConfig()
+
 	http.HandleFunc("/", handler.HandleDefaultPage)
 	http.Handle("/{filename}", http.FileServer(http.Dir("./www")))
 	http.Handle("/heroes/{filename}", http.FileServer(http.Dir("./www")))
 	http.HandleFunc("/ws", handler.HandleWs)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	log.Fatal(http.Serve(ln, nil))
+}
+
+func listenerConfig() net.Listener {
+	lc := net.ListenConfig{
+		Control: func(network, address string, conn syscall.RawConn) error {
+			var operr error
+			if err := conn.Control(func(fd uintptr) {
+				operr = syscall.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+			}); err != nil {
+				return err
+			}
+			return operr
+		},
+	}
+
+	ln, err := lc.Listen(context.Background(), "tcp", "0.0.0.0:8080")
+	if err != nil {
+		panic(err)
+	}
+
+	return ln
 }
